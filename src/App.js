09d0803fb16cb1e6e758ba3c4cf8af195bd58ec7
Karunabhupathiraju <Karuna.bhupathiraju7@gmail.com>
2024-09-02 09:46:45 -0400

    Integrate Instant Consultation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,8 +21,17 @@ function App() {
         >
           Learn React
         </a>
+        {/* Add Link to navigate to Instant Consultation */}
+        <Link to="/instant-consultation">
+            <button>Instant Booking Consultation</button>
+          </Link>
       </header>
+      <Routes>
+          {/* Define the route for Instant Consultation */}
+          <Route path="/instant-consultation" element={<InstantConsultation />} />
+        </Routes>
     </div>
+    </Router>
   );
 }
 
